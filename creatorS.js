3c44ecd448b30de4b94dd6cb264a65ae704e79c5
Nathaly Villamor <nathalyvillamor6@gmail.com>
2024-03-04 16:18:32 -0300

    adding generator for new S size

---
+++ creatorS.js
@@ -0,0 +1,112 @@
+// const QRCode = require("easyqrcodejs-nodejs");
+import QRCode from "easyqrcodejs-nodejs";
+import { v4 as uuidv4 } from "uuid";
+import { pollitos, monstruitos, heladitos, neutro, test } from "./variablesS.js";
+import { clean } from "./cleaner.js";
+
+//env variables
+const basicURL = "https://app.guapetones.ar/pets/";
+const designs = [pollitos, heladitos, monstruitos, neutro];
+
+// Options builder DO NOT TOUCH
+const options_object = (current, url) => {
+  return {
+    text: url,
+    width: 1000,
+    height: 1000,
+    colorDark: "#000000",
+    colorLight: "#ffffff",
+    correctLevel: QRCode.CorrectLevel.H,
+
+    // ====== Quiet Zone
+    quietZone: 60,
+    quietZoneColor: "white",
+
+    // ====== Logo
+    logo: current.logo, // Relative address, relative to `easy.qrcode.min.js`
+    logoWidth: 450, // fixed logo width. default is `width/3.5`
+    logoHeight: 450, // fixed logo height. default is `heigth/3.5`
+    // logoMaxWidth: undefined, // Maximum logo width. if set will ignore `logoWidth` value
+    // logoMaxHeight: undefined, // Maximum logo height. if set will ignore `logoHeight` value
+    logoBackgroundColor: "#fff", // Logo backgroud color, Invalid when `logBgTransparent` is true; default is '#ffffff'
+    logoBackgroundTransparent: true, // Whether use transparent image, default is false
+
+    // ====== Title
+    title: "_", // content
+    titleFont: "normal normal bold 10px Urbanist", //font. default is "bold 16px Arial"
+    titleColor: current.titleColor, // color. default is "#000"
+    titleBackgroundColor: "white", // background color. default is "#fff"
+    titleHeight: 120, // height, including subTitle. default is 0 (que tan pegado queda el titulo del qr. Entre mas peque mas despegado y entre mas grande mas pegado dah!)
+    titleTop: 30, // draws y coordinates. default is 30 (Entre mas pequeño mas arriba queda el titulo)
+
+    // ====== SubTitle
+    subTitle: "Ayuda, estoy perdido", // content
+    subTitleFont: "normal normal normal 100px Urbanist", // font. default is "14px Arial"
+    subTitleColor: current.subTitleColor, // color. default is "4F4F4F"
+    subTitleTop: 85, // draws y coordinates. default is 0 (que tan pegado queda el sub titulo del qr. Entre mas peque mas despegado)
+
+    // ==== CORS
+
+    // crossOrigin: null, // String which specifies the CORS setting to use when retrieving the image. null means that the crossOrigin attribute is not set.
+
+    // =====  Drawing method
+
+    // drawer: 'svg', // Which drawing method to use. 'canvas', 'svg'. default is 'canvas'
+
+    // =====  UTF-8 without BOM
+    utf8WithoutBOM: true,
+  };
+};
+
+// It generates 1 QR code
+const generate = function (name = "test", design = test, url = test.url, path = '') {
+  // New instance with options
+  var qrcode = new QRCode(options_object(design, url));
+
+  //  Save PNG to file
+  // Use png over svg cause svg use different configs
+  qrcode
+    .saveImage({
+      path: path + "q-" + name + ".png", // file path
+    })
+    .then((data) => {
+      console.log(`${name} has been Created!`);
+    });
+};
+
+// It generates many QR code of a design
+const generateManyOf = (cant, design, path) => {
+  for (let index = 0; index < cant; index++) {
+    const currentDate = new Date();
+    const timestamp = currentDate.getTime();
+    const myuuid = uuidv4();
+    const name = myuuid + "=" + timestamp + "-" + design.name;
+    const fullUrl = basicURL + name;
+
+    generate(name, design, fullUrl, path);
+  }
+};
+
+function build(cant) {
+  // It generates many QR code of each design for XS size
+  const xs = 'xs'
+  const s = 's'
+  const ml = 'ml'
+  designs.forEach((design) => {
+    let path = "generated" + "/" + xs + '/'
+    generateManyOf(cant, design, path);
+    path = "generated" + "/" +  s + '/'
+    generateManyOf(cant, design, path);
+    path = "generated" + "/" + ml + '/'
+    generateManyOf(cant, design, path);
+  });
+}
+
+//Use this to remove all the generated QRs
+clean()
+
+//Use this to build an amount of QRs
+// generate()
+console.log('generando...')
+build(5);
+// generate('instructions', neutro, 'https://app.guapetones.ar/qr/instrucciones', '');
---
+++ variablesS.js
@@ -0,0 +1,38 @@
+
+const pollitos = {
+  titleColor: "#1bb0be",
+  subTitleColor: "#dd3769",
+  logo: "logos/s/pollito.png",
+  name: "pollitos",
+};
+
+const heladitos = {
+  titleColor: "#cc6176",
+  subTitleColor: "#8f687f",
+  logo: "logos/s/heladito.png",
+  name: "heladitos",
+};
+
+const monstruitos = {
+  titleColor: "#7475a4",
+  subTitleColor: "#e9685a",
+  logo: "logos/s/monstruito.png",
+  name: "monstruitos",
+};
+
+const neutro = {
+  titleColor: "#68458c",
+  subTitleColor: "#8b8c37",
+  logo: "logos/s/neutro.png",
+  name: "neutro",
+};
+
+const test = {
+  titleColor: "#68458c",
+  subTitleColor: "#8b8c37",
+  logo: "logos/s/neutro.png",
+  name: "neutro",
+  url: "https://app.guapetones.ar/pets/q-d45f1ccf-be9f-4825-8dfe-565e933bd20c=1705509910784-neutro",
+};
+
+export { pollitos, heladitos, monstruitos, neutro, test }
